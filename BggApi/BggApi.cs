a7fa19234ebdb4ef9150264248c13cf3b2b62f85
jeradrose <jader@jader201.com>
2023-01-27 21:04:59 -0500

    Clean up + add retries to all BggClient calls

--- BggApi/BggApi.cs
+++ BggApi/BggApi.cs
@@ -11,137 +11,101 @@ namespace BggApi;
 public class BggClient {
     private static readonly HttpClient _client = new HttpClient();
 
-    public async Task<List<BoardGame>> GetBoardGamesAsync(IEnumerable<int> gameIds) {
+    public async Task<List<BoardGame>?> GetBoardGamesAsync(IEnumerable<int> gameIds) {
         var gameIdList = string.Join(',', gameIds.Distinct().Order());
         var cacheKey = $"{nameof(GetBoardGamesAsync)}-{gameIdList}";
+        var url = $"https://boardgamegeek.com/xmlapi2/thing?id={gameIdList}&stats=1";
 
-        var (xml, dateString) = await GetOrSetXmlAndDateCache(cacheKey, $"https://boardgamegeek.com/xmlapi2/thing?id={gameIdList}&stats=1");
-
-        var serializer = new XmlSerializer(
-            typeof(List<BoardGame>), 
-            new XmlRootAttribute("items")
-        );
-
-        using var sr = new StringReader(xml);
-
-        var boardGames = (List<BoardGame>?)serializer.Deserialize(sr);
-
-        boardGames?.ForEach(g => g.CacheDate = DateTime.Parse(dateString));
-
-        if (boardGames == null) {
-            return new List<BoardGame>();
-        }
-
-        return boardGames;
+        return await GetOrSetXmlAndDateCache<List<BoardGame>?>(cacheKey, url, "items");
     }
 
-    public async Task<List<CollectionItem>> GetCollectionAsync(string username) {
-        var cache = new SqliteCache(new SqliteCacheOptions());
+    public async Task<List<CollectionItem>?> GetCollectionAsync(string username) {
         var cacheKey = $"{nameof(GetCollectionAsync)}-{username}";
-        var cacheDateKey = $"{cacheKey}-date";
+        var url = $"https://boardgamegeek.com/xmlapi2/collection?username={username}&stats=1";
 
-        var xml = cache.GetString(cacheKey);
-        var dateString = cache.GetString(cacheDateKey);
-
-        if (string.IsNullOrEmpty(xml) || string.IsNullOrEmpty(dateString)) {
-            const int maxRetries = 10;
-
-            async Task<string> apiCallAsync() {
-                try {
-                    return await _client.GetStringAsync($"https://boardgamegeek.com/xmlapi2/collection?username={username}&stats=1"); //&excludesubtype=boardgameexpansion
-                } catch (Exception ex) {
-                    throw new BggException($"Exception calling BGG API after {maxRetries} attempts: {ex.Message}");
-                }
-            }
-
-            xml = await apiCallAsync();
-            var tries = 0;
-
-            while (xml.Contains("Please try again later for access.") && tries <= maxRetries) {
-                if (tries == maxRetries) {
-                    throw new BggException("Too many attempts.");
-                }
-                tries++;
-                SystemThread.Sleep(1000 * tries); // Add another second delay for each retry
-                xml = await apiCallAsync();
-            }
-
-            if (xml.Contains("Invalid username")) {
-                return new List<CollectionItem>();
-            }
-
-            dateString = DateTime.UtcNow.ToString();
-
-            cache.SetString(cacheKey, xml);
-            cache.SetString(cacheDateKey, dateString);
-        }
-
-        var serializer = new XmlSerializer(
-            typeof(List<CollectionItem>),
-            new XmlRootAttribute("items")
-        );
-
-        using var sr = new StringReader(xml);
-
-        var collection = (List<CollectionItem>?)serializer.Deserialize(sr);
-
-        collection?.ForEach(g => g.CacheDate = DateTime.Parse(dateString));
-
-        if (collection == null) {
-            return new List<CollectionItem>();
-        }
-
-        return collection;
+        return await GetOrSetXmlAndDateCache<List<CollectionItem>?>(cacheKey, url, "items");
     }
 
     public async Task<Thread?> GetThreadAsync(int threadId) {
         var cacheKey = $"{nameof(GetThreadAsync)}-{threadId}";
+        var url = $"https://boardgamegeek.com/xmlapi2/thread?id={threadId}";
 
-        var (xml, dateString) = await GetOrSetXmlAndDateCache(cacheKey, $"https://boardgamegeek.com/xmlapi2/thread?id={threadId}");
-
-        var serializer = new XmlSerializer(
-            typeof(Thread),
-            new XmlRootAttribute("thread")
-        );
-
-        using var sr = new StringReader(xml);
-
-        var thread = (Thread?)serializer.Deserialize(sr);
-
-        return thread;
+        return await GetOrSetXmlAndDateCache<Thread?>(cacheKey, url, "thread");
     }
 
     public async Task<GeekList?> GetGeekListAsync(int geekListId) {
         var cacheKey = $"{nameof(GetGeekListAsync)}-{geekListId}";
+        var url = $"https://boardgamegeek.com/xmlapi/geeklist/{geekListId}";
 
-        var (xml, dateString) = await GetOrSetXmlAndDateCache(cacheKey, $"https://boardgamegeek.com/xmlapi/geeklist/{geekListId}");
-
-        var serializer = new XmlSerializer(
-            typeof(GeekList),
-            new XmlRootAttribute("geeklist")
-        );
-
-        using var sr = new StringReader(xml);
-
-        var geekList = (GeekList?)serializer.Deserialize(sr);
-
-        return geekList;
+        return await GetOrSetXmlAndDateCache<GeekList?>(cacheKey, url, "geeklist");
     }
 
-    private async Task<(string, string)> GetOrSetXmlAndDateCache(string cacheKey, string url) {
+    private async Task<T?> GetOrSetXmlAndDateCache<T>(string cacheKey, string url, string xmlRoot) where T : new() {
         var cache = new SqliteCache(new SqliteCacheOptions());
         var cacheDateKey = $"{cacheKey}-date";
 
         var xml = cache.GetString(cacheKey);
         var dateString = cache.GetString(cacheDateKey);
 
-        if (string.IsNullOrEmpty(xml) || string.IsNullOrEmpty(dateString)) {
-            xml = await _client.GetStringAsync(url);
-            dateString = DateTime.UtcNow.ToString();
-            cache.SetString(cacheKey, xml, new DistributedCacheEntryOptions { });
-            cache.SetString(cacheDateKey, dateString);
+        var response = default(T);
+
+        if (!string.IsNullOrEmpty(xml) && !string.IsNullOrEmpty(dateString)) {
+            response = SerializeXmlSafe<T?>(xml, xmlRoot);
+            if (response != null) {
+                return response;
+            }
         }
 
-        return (xml, dateString);
+        const int maxRetries = 10;
+
+        async Task<string> apiCallAsync() {
+            try {
+                return await _client.GetStringAsync(url);
+            } catch (Exception ex) {
+                throw new BggException($"Exception calling BGG API after {maxRetries} attempts: {ex.Message}");
+            }
+        }
+
+        xml = await apiCallAsync();
+        var tries = 0;
+
+        while (xml.Contains("Please try again later for access.") && tries <= maxRetries) {
+            if (tries == maxRetries) {
+                throw new BggException("Too many attempts.");
+            }
+            tries++;
+            SystemThread.Sleep(1000 * tries); // Add another second delay for each retry
+            xml = await apiCallAsync();
+        }
+
+        if (xml.Contains("Invalid username")) {
+            return default;
+        }
+
+        dateString = DateTime.UtcNow.ToString();
+
+        cache.SetString(cacheKey, xml);
+        cache.SetString(cacheDateKey, dateString);
+
+        return SerializeXml<T?>(xml, xmlRoot);
+    }
+
+    private T? SerializeXmlSafe<T>(string xml, string xmlRoot) where T : new() {
+        try {
+            return SerializeXml<T>(xml, xmlRoot);
+        } catch {
+            return default;
+        }
+    }
+
+    private T? SerializeXml<T>(string xml, string xmlRoot) where T : new() {
+        var serializer = new XmlSerializer(
+            typeof(T),
+            new XmlRootAttribute(xmlRoot)
+        );
+
+        using var sr = new StringReader(xml);
+
+        return (T?)serializer.Deserialize(sr);
     }
 }
